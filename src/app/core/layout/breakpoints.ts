371e66958c275ac7d2841effabc3dd9310395458
Your Name <you@example.com>
2025-12-31 14:31:20 +0100

    Refactor: introduce layout component + desktop navbar

---
+++ src/app/core/layout/breakpoints.ts
@@ -0,0 +1,5 @@
+export const BREAKPOINTS = { 
+    phone: '(max-width: 599px)', 
+    tablet: '(min-width: 600px) and (max-width: 1023px)', 
+    desktop: '(min-width: 1024px)' 
+};
---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { BREAKPOINTS } from './breakpoints';
+import { NavbarDesktopComponent } from "../../components/navbar-desktop/navbar-desktop";
+import { FooterComponent } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  imports: [
+    NavbarDesktopComponent,
+    RouterOutlet,
+    FooterComponent
+]
+})
+export class LayoutComponent {
+  isPhone = false;
+  isTablet = false;
+  isDesktop = false;
+
+  constructor(private bp: BreakpointObserver) {
+    this.bp.observe([
+      BREAKPOINTS.phone,
+      BREAKPOINTS.tablet,
+      BREAKPOINTS.desktop
+    ]).subscribe(result => {
+      this.isPhone = result.breakpoints[BREAKPOINTS.phone];
+      this.isTablet = result.breakpoints[BREAKPOINTS.tablet];
+      this.isDesktop = result.breakpoints[BREAKPOINTS.desktop];
+    });
+  }
+}
